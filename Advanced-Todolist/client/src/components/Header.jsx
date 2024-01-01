27bdf875a420a75e02785a6f0fe7feda5dc3004c
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2024-01-01 11:38:19 +0500

    Add files via upload

--- Advanced-Todolist/client/src/components/Header.jsx
+++ Advanced-Todolist/client/src/components/Header.jsx
@@ -14,14 +14,14 @@ const Header = () => {
          
         <div>
   <header className="text-gray-700 bg-gray-200 body-font flex items-center">
-    <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center justify-between">
+    <div className="container mx-auto flex flex-wrap p-0 flex-col md:flex-row items-center justify-between">
       <a className="flex title-font font-medium items-center text-gray-900 mb-2 md:mb-0">
         <img src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/TODO-Group-900x0.png" alt="sfsf" width={120} height={120}  />
         {/* <span className="ml-3 text-xl">TodoApp</span> */}
       </a>
       <div className="flex space-x-5 items-center mt-4">
         {token ? (
-          <Link to={"/login"} className="inline-flex items-center bg-slate-500 text-white border-0 py-2 px-3 focus:outline-none hover:bg-gray-700 rounded text-base md:mt-0" onClick={handleLogout}>
+          <Link to={"/login"} className="inline-flex items-center bg-white text-black border-0 py-2 px-3 focus:outline-none hover:bg-gray-700 rounded text-base md:mt-0" onClick={handleLogout}>
             Logout
           </Link>
         ) : (
